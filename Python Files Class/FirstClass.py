909aa21f5eedc1d11bae75d516f96203c57f1da6
Yuri Mendes <61084692+Yuri-Mendes@users.noreply.github.com>
2023-06-26 22:10:46 -0300

    commit class01

---
+++ Python Files Class/FirstClass.py
@@ -0,0 +1,19 @@
+#Primeira aula#
+#Hello Fucking World#
+print('hello fucking world')
+
+print('this a calculator')
+#variáveis#
+num=50
+num2=20
+
+#condição#
+res1=num+num2
+res2=num/num2
+res3=num*num2
+res4=num-num2
+
+print(f'Resultado da soma dos valores: {res1}')
+print(f'Resultado da divisão dos valores: {res2}')
+print(f'Resultado da multiplicação dos valores: {res3}')
+print(f'Resultado da divisão dos valores: {res4}')
