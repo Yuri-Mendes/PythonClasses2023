1327ca5f49447376ad44efa6d09b610bcfd13c5c
Yuri Mendes <61084692+Yuri-Mendes@users.noreply.github.com>
2023-06-28 16:48:51 -0300

    First Commit

---
+++ Python Files Classes/Calculadora.py
@@ -0,0 +1,10 @@
+print('This a Calculator')
+#variáveis
+num1 = input('Informe um número:')
+num2 = input('Informe um número:')
+
+#condição
+soma = int(num1) + int(num2)
+
+#resultado
+print('O resultado da soma dos valores é igual a {}' . format(soma))
